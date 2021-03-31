93fc810a79878e736d1e2f8371cc607eae2162f4
Angel <asimon@docentes.frgp.utn.edu.ar>
2021-03-31 18:01:16 -0300

    Archivos de la clase 01

---
+++ Clase 01/ejemplo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n1;
+    int n2;
+    int suma;
+    cout << "Ingresar el primer número: ";
+    cin >> n1;
+    cout << "Ingresar el segundo número: ";
+    cin >> n2;
+    suma = n1 + n2;
+    cout << "El resultado de la suma es: ";
+    cout << suma;
+
+    return 0;
+}
